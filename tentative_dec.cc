53bca7f18a82db9188d3d59dbf36cf1422eb1594
Simon Spannagel <simon.spannagel@desy.de>
2013-09-11 15:59:17 +0200

    slight changes to tentativedec tool: now checks timestamps to be monotonically increasing, added "flags" variable.

--- tentative_dec.cc
+++ tentative_dec.cc
@@ -16,16 +16,25 @@ int main(int argc, char* argv[]) {
   CMSPixelStatistics global_statistics;
   std::vector<pixel> * evt = new std::vector<pixel>;
   int64_t timestamp = 0;
+  int64_t old_timestamp = 0;
+  int flags = 0; //FLAG_OLD_RAL_FORMAT;
 
   int events = atoi(argv[2]);
   std::cout << "Decoding " << events << " events." << std::endl;
 
   for (int i = 3; i < argc; ++i) {
     std::cout << "Trying to decode " << argv[i] << std::endl;
-    CMSPixelFileDecoder * decoder = new CMSPixelFileDecoderRAL(argv[i],8,0,ROC_PSI46DIG);
+    CMSPixelFileDecoder * decoder = new CMSPixelFileDecoderRAL(argv[i],8,flags,ROC_PSI46DIGV2);
 
-    for(int j = 0; j < events; ++j)
+    for(int j = 0; j < events; ++j) {
       if(decoder->get_event(evt, timestamp) <= DEC_ERROR_NO_MORE_DATA) break;
+
+      if(timestamp < old_timestamp) {
+	LOG(logWARNING) << "Timestamps not monotonically increasing!";
+      }
+      old_timestamp = timestamp;
+
+    }
 
     global_statistics.update(decoder->statistics);
     decoder->statistics.print();
