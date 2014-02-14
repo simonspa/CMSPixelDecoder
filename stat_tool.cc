55046cbb736e325bdd7cad440aab3ecdf860e3c0
Simon Spannagel <simonspa@kth.se>
2014-02-14 23:08:20 +0100

    Rework stat_tool a bit to allow proper CLI arguments: -v LEVEL: sets verbosity -n NUM: sets number of expected ROCs everything else will be treated as name of a file to be decoded.

--- stat_tool.cc
+++ stat_tool.cc
@@ -11,16 +11,27 @@ using namespace CMSPixel;
 
 int main(int argc, char* argv[]) {
 
-  Log::ReportingLevel() = Log::FromString(argv[1] ? argv[1] : "SUMMARY");
+  Log::ReportingLevel() = Log::FromString("SUMMARY");
 
   CMSPixelStatistics global_statistics;
   std::vector<pixel> * evt = new std::vector<pixel>;
   timing time;
+  unsigned int num_rocs = 8;
+  std::vector<std::string> files;
 
-  for (int i = 2; i < argc; ++i) {
+  for (int i = 1; i < argc; i++) {
+    // Setting number of expected ROCs:
+    if (!strcmp(argv[i],"-n")) { num_rocs = atoi(argv[++i]); }
+    // Set the verbosity level:
+    else if (!strcmp(argv[i],"-v")) { Log::ReportingLevel() = Log::FromString(argv[++i]); }
+    // add to the list of files to be processed:
+    else { files.push_back(string(argv[i])); }
+  }
 
-    std::cout << "Trying to decode " << argv[i] << std::endl;
-    CMSPixelFileDecoder * decoder = new CMSPixelFileDecoderRAL(argv[i],8,0,ROC_PSI46DIGV2);
+  for (std::vector<std::string>::iterator it = files.begin(); it != files.end(); ++it) {
+    
+    std::cout << "Trying to decode " << (*it) << std::endl;
+    CMSPixelFileDecoder * decoder = new CMSPixelFileDecoderRAL((*it).c_str(),num_rocs,0,ROC_PSI46DIGV2);
 
     while(1)
       if(decoder->get_event(evt, time) <= DEC_ERROR_NO_MORE_DATA) break;
