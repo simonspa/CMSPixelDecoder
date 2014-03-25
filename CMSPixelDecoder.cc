95fbfd195ab3270325d25bfc22b1afcb2b14ba46
Simon Spannagel <simon.spannagel@desy.de>
2014-03-25 18:51:11 +0100

    Fix bug in stat_tool badevent filter and remove some unnecessary debug output. Bump version to 3.7.0

--- CMSPixelDecoder.cc
+++ CMSPixelDecoder.cc
@@ -563,22 +563,17 @@ bool CMSPixelFileDecoderPSI_DTB::chop_datastream(std::vector< uint16_t > * rawda
   
   // Store the first header word:
   rawdata->push_back(word);
-  LOG(logDEBUG4) << "Add first: " << std::hex << word << std::dec << std::endl;      
 
   // read the data until the next MTB header arises:
   // morewords:
   if(!readWord(word)) return false;
   while( !((word&0xF000) > 0x0000) && !feof(mtbStream)){
     rawdata->push_back(word);
-    LOG(logDEBUG4) << "Add " << std::hex << word << std::dec << std::endl;      
     if(!readWord(word)) return false;
   }
 
   // Store the last data word:
-  if((word&0xF000) == 0x4000) {
-     LOG(logDEBUG4) << "Add " << std::hex << word << std::dec << std::endl;      
-     rawdata->push_back(word);
-  }
+  if((word&0xF000) == 0x4000) { rawdata->push_back(word); }
   else {
      // Rewind one word to detect the header correctly later on:
      fseek(mtbStream , -2 , SEEK_CUR);
